0cdfc5e9fd4a1354a381fa4f1c8c36da3a73cc84
MaxtuneLee <max@xox.im>
2024-10-23 16:47:09 +0800

    chore: update package manager version and enhance Tailwind CSS configuration

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,78 +1,82 @@
 function withOpacity(variableName) {
-  return ({ opacityValue }) => {
-    if (opacityValue !== undefined) {
-      return `rgba(var(${variableName}), ${opacityValue})`;
-    }
-    return `rgb(var(${variableName}))`;
-  };
+	return ({ opacityValue }) => {
+		if (opacityValue !== undefined) {
+			return `rgba(var(${variableName}), ${opacityValue})`;
+		}
+		return `rgb(var(${variableName}))`;
+	};
 }
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    // Remove the following screen breakpoint or add other breakpoints
-    // if one breakpoint is not enough for you
-    screens: {
-      sm: "640px",
-    },
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	theme: {
+		// Remove the following screen breakpoint or add other breakpoints
+		// if one breakpoint is not enough for you
+		screens: {
+			sm: "640px",
+			md: "768px",
+			lg: "1024px",
+			xl: "1280px",
+			"2xl": "1536px",
+		},
 
-    extend: {
-      textColor: {
-        skin: {
-          base: withOpacity("--color-text-base"),
-          accent: withOpacity("--color-accent"),
-          inverted: withOpacity("--color-fill"),
-        },
-      },
-      backgroundColor: {
-        skin: {
-          fill: withOpacity("--color-fill"),
-          accent: withOpacity("--color-accent"),
-          inverted: withOpacity("--color-text-base"),
-          card: withOpacity("--color-card"),
-          "card-muted": withOpacity("--color-card-muted"),
-        },
-      },
-      outlineColor: {
-        skin: {
-          fill: withOpacity("--color-accent"),
-        },
-      },
-      borderColor: {
-        skin: {
-          line: withOpacity("--color-border"),
-          fill: withOpacity("--color-text-base"),
-          accent: withOpacity("--color-accent"),
-        },
-      },
-      fill: {
-        skin: {
-          base: withOpacity("--color-text-base"),
-          accent: withOpacity("--color-accent"),
-        },
-        transparent: "transparent",
-      },
-      fontFamily: {
-        mono: ["Noto Sans SC", "JetBrains Mono", "monospace"],
-        // sans: ["Noto Sans SC", "sans-serif"],
-      },
+		extend: {
+			textColor: {
+				skin: {
+					base: withOpacity("--color-text-base"),
+					accent: withOpacity("--color-accent"),
+					inverted: withOpacity("--color-fill"),
+				},
+			},
+			backgroundColor: {
+				skin: {
+					fill: withOpacity("--color-fill"),
+					accent: withOpacity("--color-accent"),
+					inverted: withOpacity("--color-text-base"),
+					card: withOpacity("--color-card"),
+					"card-muted": withOpacity("--color-card-muted"),
+				},
+			},
+			outlineColor: {
+				skin: {
+					fill: withOpacity("--color-accent"),
+				},
+			},
+			borderColor: {
+				skin: {
+					line: withOpacity("--color-border"),
+					fill: withOpacity("--color-text-base"),
+					accent: withOpacity("--color-accent"),
+				},
+			},
+			fill: {
+				skin: {
+					base: withOpacity("--color-text-base"),
+					accent: withOpacity("--color-accent"),
+				},
+				transparent: "transparent",
+			},
+			fontFamily: {
+				mono: ["Noto Sans SC", "JetBrains Mono", "monospace"],
+				// sans: ["Noto Sans SC", "sans-serif"],
+			},
 
-      typography: {
-        DEFAULT: {
-          css: {
-            pre: {
-              color: true,
-              fontFamily: "JetBrains Mono"
-            },
-            code: {
-              color: true,
-              fontFamily: "JetBrains Mono"
-            },
-          },
-        },
-      },
-    },
-  },
-  plugins: [require("@tailwindcss/typography")],
+			typography: {
+				DEFAULT: {
+					css: {
+						pre: {
+							color: true,
+							fontFamily: "JetBrains Mono",
+						},
+						code: {
+							color: true,
+							fontFamily: "JetBrains Mono",
+						},
+					},
+				},
+			},
+		},
+	},
+	plugins: [require("@tailwindcss/typography")],
 };
